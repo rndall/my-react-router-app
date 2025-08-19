09216a19298189c2c2b43475037995c25cff2a68
rndall <96964503+rndall@users.noreply.github.com>
2025-08-19 16:52:24 +0800

    feat: add search spinner

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -21,6 +21,11 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
   const { contacts, q } = loaderData;
   const navigation = useNavigation();
   const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
+  console.log(navigation);
 
   const [query, setQuery] = useState(q || "");
 
@@ -42,6 +47,7 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
           >
             <input
               aria-label="Search contacts"
+              className={searching ? "loading" : ""}
               value={query}
               onChange={({ target }) => setQuery(target.value)}
               id="q"
@@ -49,7 +55,7 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
               placeholder="Search"
               type="search"
             />
-            <div aria-hidden hidden={true} id="search-spinner" />
+            <div aria-hidden hidden={!searching} id="search-spinner" />
           </Form>
           <Form method="post">
             <button type="submit">New</button>
@@ -81,7 +87,9 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
         </nav>
       </div>
       <div
-        className={navigation.state === "loading" ? "loading" : ""}
+        className={
+          navigation.state === "loading" && !searching ? "loading" : ""
+        }
         id="detail"
       >
         <Outlet />
