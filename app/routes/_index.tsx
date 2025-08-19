063abf9c5c3874fc0cf6ee91f95a843500434579
rndall <96964503+rndall@users.noreply.github.com>
2025-08-19 13:02:54 +0800

    feat: create layout routes and pre-prender about route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,5 @@
-export default function Home() {
-  return (
-    <p id="index-page">
-      This is a demo for React Router.
-      <br />
-      Check out{" "}
-      <a href="https://reactrouter.com">the docs at reactrouter.com</a>.
-    </p>
-  );
+import { Outlet } from "react-router";
+
+export default function Index() {
+  return <Outlet />;
 }
